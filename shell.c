258ebeeb4ecb1e304e4932d3d467d7a658c5b99b
Tharealpixel <medboutahor@gmail.com>
2023-05-13 12:49:54 +0100

    W

--- shell.c
+++ shell.c
@@ -1,54 +1,131 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
 
-void display_prompt()
+#define MAX_LINE 80
+
+char *read_line(void)
 {
-	printf("#cisfun$ ");
+	int bufsize = MAX_LINE;
+	int position = 0;
+	char *buffer = malloc(sizeof(char) * bufsize);
+	int c;
+
+	if (!buffer)
+	{
+		perror("shell: allocation error");
+		exit(EXIT_FAILURE);
+	}
+
+	while (1)
+	{
+		c = getchar();
+
+		if (c == EOF || c == '\n')
+		{
+			buffer[position] = '\0';
+			return buffer;
+		}
+		else
+		{
+			buffer[position] = c;
+		}
+		position++;
+
+		if (position >= bufsize)
+		{
+			bufsize += MAX_LINE;
+			buffer = realloc(buffer, bufsize);
+			if (!buffer)
+			{
+				perror("shell: allocation error");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
+
+char **split_line(char *line)
+{
+	int bufsize = MAX_LINE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
+
+	if (!tokens)
+	{
+		perror("shell: allocation error");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, " \t\r\n\a");
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
+
+		if (position >= bufsize)
+		{
+			bufsize += MAX_LINE;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				perror("shell: allocation error");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		token = strtok(NULL, " \t\r\n\a");
+	}
+	tokens[position] = NULL;
+	return tokens;
+}
+
+void launch_process(char **args)
+{
+	pid_t pid, wpid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror("shell");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		perror("shell");
+	}
+	else
+	{
+		do
+		{
+			wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
 }
 
 int main(void)
 {
-	pid_t pid;
 	char *line;
+	char **args;
+	int status;
 
-	while (1)
+	do
 	{
-		printf("#cisfun$ ");
+		printf("shell> ");
 		line = read_line();
-
-		pid = fork();
-		if (pid < 0)
-		{
-			perror("fork error");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			/* child process */
-			execve(line, NULL, environ);
-			perror("execve error");
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			/* parent process */
-			wait(NULL);
-		}
+		args = split_line(line);
+		launch_process(args);
 
 		free(line);
-	}
+		free(args);
+	} while (status);
 
-	return EXIT_SUCCESS;
-}
-
-char *read_line(void)
-{
-	char *line = NULL;
-	size_t bufsize = 0;
-	ssize_t linelen = getline(&line, &bufsize, stdin);
-	if (linelen == -1)
-	{
-		perror("getline");
-		exit(EXIT_FAILURE);
-	}
-	return line;
+	return 0;
 }
