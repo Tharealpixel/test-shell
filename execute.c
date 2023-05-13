5770ea307f60f31724be5b90f91ee52b373feaf0
Tharealpixel <medboutahor@gmail.com>
2023-05-13 13:39:24 +0100

    W

--- execute.c
+++ execute.c
@@ -8,53 +8,85 @@
  */
 int execute_command(char *command)
 {
-    char **args, *token;
-    int i = 0;
-    pid_t pid;
-    int status;
+	char **args, *token;
+	int i = 0;
+	pid_t pid;
+	int status;
 
-    args = malloc(sizeof(char *) * BUFFER_SIZE);
-    if (!args)
-        return (-1);
+	args = malloc(sizeof(char *) * BUFFER_SIZE);
+	if (!args)
+		return (-1);
 
-    token = _strtok(command, " ");
-    while (token)
-    {
-        args[i] = _strdup(token);
-        if (!args[i])
-        {
-            free(args);
-            return (-1);
-        }
-        token = _strtok(NULL, " ");
-        i++;
-    }
-    args[i] = NULL;
+	token = _strtok(command, " ");
+	while (token)
+	{
+		args[i] = _strdup(token);
+		if (!args[i])
+		{
+			free(args);
+			return (-1);
+		}
+		token = _strtok(NULL, " ");
+		i++;
+	}
+	args[i] = NULL;
 
-    pid = fork();
-    if (pid == -1)
-    {
-        free(args);
-        return (-1);
-    }
+	pid = fork();
+	if (pid == -1)
+	{
+		free(args);
+		return (-1);
+	}
 
-    if (pid == 0)
-    {
-        if (execve(args[0], args, NULL) == -1)
-        {
-            free(args);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        do {
-            waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+	if (pid == 0)
+	{
+		if (execve(args[0], args, NULL) == -1)
+		{
+			free(args);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		do
+		{
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
 
-    free(args);
-    return (0);
+	free(args);
+	return (0);
 }
+char *get_input(void)
+{
+	char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
+	ssize_t nread;
 
+	if (buffer == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
 
+	nread = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+	if (nread == -1)
+	{
+		perror("read");
+		exit(EXIT_FAILURE);
+	}
+
+	if (buffer[nread - 1] == '\n')
+	{
+		buffer[nread - 1] = '\0';
+		nread--;
+	}
+
+	buffer = realloc(buffer, sizeof(char) * nread);
+	if (buffer == NULL)
+	{
+		perror("realloc");
+		exit(EXIT_FAILURE);
+	}
+
+	return (buffer);
+}
